d93c91f974d5499b83e75cda0dfbce2ee8bd0511
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-11-25 01:30:06 +0000

    Updated eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -8,24 +8,19 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
 
-    let i = 0;
-    let answer = Boolean;
-  
-    for (i = 0; i < arr1.length; i++) {
-  
-      if (arr1.length === arr2.length) {
-        if (arr1[i] === arr2[i]) {
-          answer = true;
-        } else if (arr1[i] !== arr2[i]) {
-          answer = false;
-        }
-      } else {
-        answer = false
-      }
+ 
+    if (arr1.length !== arr2.length) {
+        return false;
     }
-    return answer;
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+    return true;
   
-  };
+};
   
   module.exports = eqArrays;
 
