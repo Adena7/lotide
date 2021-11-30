85818c2a500bf03446ed6c0da4e7be20274b4425
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-11-30 03:08:10 +0000

    new chai and mocha refactor

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,15 +5,15 @@ const eqArrays = require('../eqArrays');
 
 describe("#middle", () => {
     it("returns [2, 3] for [1, 2, 3, 4]", () => {
-      assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+      assert.deepEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
     });
   
     it("returns [2] for [1, 2, 3]", () => {
-      assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
+      assert.deepEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
     });
   
     it("returns [5] for [1, 2, 3, 4, 5, 6, 7, 8, 9]", () => {
-      assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]));
+      assert.deepEqual(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]));
     });
   });
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -4,15 +4,15 @@ const tail   = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 describe("#tail", () => {
   it("returns 2 for length", () => {
-    assert.strictEqual(result.length, 2);
+    assert.deepEqual(result.length, 2);
   });
 
   it("returns 'Lighthouse' as first string of tail(['Hello', 'Lighthouse', 'Labs'])", () => {
-    assert.strictEqual(result[0], "Lighthouse"); 
+    assert.deepEqual(result[0], "Lighthouse"); 
   });
 
   it("rreturns 'Labs' as second string of tail(['Hello', 'Lighthouse', 'Labs'])", () => {
-    assert.strictEqual(result[1], "Labs"); 
+    assert.deepEqual(result[1], "Labs"); 
   });
 
 });
