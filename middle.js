56e33d2c51100e310b6290111bcc3656c7b07126
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-11-30 02:35:53 +0000

    new changes

--- middle.js
+++ middle.js
@@ -1,28 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-
- 
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-    return true;
-  
-};
-
-  
-
-let assertArraysEqual = function(arr1, arr2) {
-    if (eqArrays (arr1, arr2)) {
-        console.log('pass');
-    } else {
-        console.log('fail');
-    }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
     
     const middle = function (array) {
     let result = [];
@@ -36,9 +13,3 @@ let assertArraysEqual = function(arr1, arr2) {
 };
 
 module.exports = middle;
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // PASS
-// assertArraysEqual(middle([1, 2, 3]), [2]); // PASS
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]); // PASS
-// assertArraysEqual(middle([1, 2, 3]), ["2"]); // FAIL
-// assertArraysEqual(middle([1, 2, 3, 4]), [3, 2]); // FAIL
