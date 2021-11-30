4688437839f7ea19f5292680d6ee528a78fb864f
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-11-30 01:46:28 +0000

    new changes

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = require('./assertEqual');
+
 
 
 const head = function(array) {
@@ -6,6 +6,6 @@ const head = function(array) {
     return array[0]
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+head(["Hello", "Lighthouse", "Labs"]);
+head([[5,6,7]]);
+module.exports = head;
