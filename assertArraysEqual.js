5ab27bda886f793208ced37a57ae9a1c661ea675
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-11-30 02:28:32 +0000

    new refactored changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,6 @@
-const eqArrays = function(arr1, arr2) {
+const eqArrays = require('./eqArrays');
 
- 
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        }
-      }
-    return true;
-  
-};
-
-let assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
     if (eqArrays (arr1, arr2)) {
         console.log('pass');
     } else {
@@ -23,8 +9,3 @@ let assertArraysEqual = function(arr1, arr2) {
 };
 
   module.exports = assertArraysEqual; 
-
-assertArraysEqual((eqArrays)([1, 2, 3], [3, 2, 1])); // => false
-assertArraysEqual((eqArrays)([1, 2, 3], [1, 2, 3])); // => true
-assertArraysEqual((eqArrays)(["1", "2", "3"], ["1", "2", "3"])); // => true
-assertArraysEqual((eqArrays)(["1", "2", "3"], ["1", "2", 3])); // => false
