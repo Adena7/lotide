526fcff7771f402ca3ce49a64ca38d9d76ca519d
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-11-30 03:05:10 +0000

    chai and mocha additions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,24 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // PASS
-assertArraysEqual(middle([1, 2, 3]), [2]); // PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]); // PASS
-assertArraysEqual(middle([1, 2, 3]), ["2"]); // FAIL
-assertArraysEqual(middle([1, 2, 3, 4]), [3, 2]); // FAIL
+describe("#middle", () => {
+    it("returns [2, 3] for [1, 2, 3, 4]", () => {
+      assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+    });
+  
+    it("returns [2] for [1, 2, 3]", () => {
+      assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
+    });
+  
+    it("returns [5] for [1, 2, 3, 4, 5, 6, 7, 8, 9]", () => {
+      assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]));
+    });
+  });
+
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // PASS
+// assertArraysEqual(middle([1, 2, 3]), [2]); // PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]); // PASS
+// assertArraysEqual(middle([1, 2, 3]), ["2"]); // FAIL
+// assertArraysEqual(middle([1, 2, 3, 4]), [3, 2]); // FAIL
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,24 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns 2 for length", () => {
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("returns 'Lighthouse' as first string of tail(['Hello', 'Lighthouse', 'Labs'])", () => {
+    assert.strictEqual(result[0], "Lighthouse"); 
+  });
+
+  it("rreturns 'Labs' as second string of tail(['Hello', 'Lighthouse', 'Labs'])", () => {
+    assert.strictEqual(result[1], "Labs"); 
+  });
+
+});
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
-assertEqual(tail(words).length, 2); // should PASS
+// assertEqual(tail(words).length, 2); // should PASS
